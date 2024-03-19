b6e197933a9f0d5a0fd1e187e2373d05165a02c6
Ibrahim Ame <ai.iot@neu.edu.tr>
2024-03-19 09:54:51 +0200

    New Team Members

--- pages/teammembers.py
+++ pages/teammembers.py
@@ -6,26 +6,28 @@ st.container(height=50, border=False)
 
 col1, col2 = st.columns(2)
 
+names = ["Prof. Dr. Tamer Şanlıdağ", "Prof. Dr. Fadi Al-Turjman", 
+         "Assoc. Prof. Dr. Cenk Serhan Özverel", "Asst. Prof. Dr. Ayşe Seyer",
+         "Dr. Erdal Sanlidag", "Mr. Ibrahim Ame", 
+         "Ms. Hadjer Benyamina", "Matin Taghianzade"]
+
+images = [ "pages/images/tamer.jpg", "pages/images/fadi.jpg", 
+         "pages/images/serhan.jpg", "pages/images/seyer.jpg",
+         "pages/images/default.png", "pages/images/ibrahim.png", 
+         "pages/images/hadjer.jpeg", "pages/images/default.png" ]
+
+counter = 0;
+
 with st.container():
+    for i in range(0, int(len(images)/2)):
 
-    columnContainer = col1.container(border = True)
-    columnContainer.image("pages/images/tamer.jpg", width=None)
-    columnContainer.header("Prof. Dr. Tamer Şanlıdağ")
+        columnContainer = col1.container(border = True)
+        columnContainer.image(images[counter], width=None)
+        columnContainer.header(names[counter])
 
-    columnContainer = col2.container(border = True)
-    columnContainer.image("pages/images/fadi.jpg", width=None)
-    columnContainer.header("Prof. Dr. Fadi Al-Turjman")
-
-    columnContainer = col1.container(border = True)
-    columnContainer.image("pages/images/serhan.jpg", width=None)
-    columnContainer.header("Assoc. Prof. Dr. Cenk Serhan Özverel")
-
-    columnContainer = col2.container(border = True)
-    columnContainer.image("pages/images/ibrahim.png", width=None)
-    columnContainer.header("Mr. Ibrahim Ame")
-
-    columnContainer = col1.container(border = True)
-    columnContainer.image("pages/images/default.png", width=None)
-    columnContainer.header("Ms. Hadjer Benyamina")
-
+        counter += 1
+        columnContainer = col2.container(border = True)
+        columnContainer.image(images[counter], width=None)
+        columnContainer.header(names[counter])
+        counter += 1
     
