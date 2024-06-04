68548495f210db02c9ca40d62a340b833e056033
Ibrahim Ame <ai.iot@neu.edu.tr>
2024-06-04 12:18:35 +0300

    Predictions API

---
+++ api/api.py
@@ -0,0 +1,54 @@
+import json
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+def preprocess_image(image_path, size):
+    img = image.load_img(image_path, target_size=(size, size))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0
+    return img_array
+
+def predictWithImage(img, model_name, size):
+    loaded_model = load_model(model_name)
+    return predict_image(loaded_model, img, size)
+
+def predict_image(model, image_path, size):
+    preprocessed_image = preprocess_image(image_path, size)
+    prediction = model.predict(preprocessed_image)
+    class_index = np.argmax(prediction)
+    class_labels = ['Hyalomma', 'Rhipicephalus', "Unidentified"]
+    predicted_class = class_labels[class_index]
+    return predicted_class
+
+application = Flask(__name__)
+CORS(application)
+
+@application.route("/predict/", methods=['GET'])
+def get_results():
+
+    image_name = request.args.get("imageName")
+    size = request.args.get("modelInputFeatureSize")
+    model_name = request.args.get("modelFilename")
+   
+    size = 32
+    image_name = "../../TicksApp/uploads/" + image_name
+    model_name = "../models/" + model_name
+
+    result = predictWithImage(image_name, model_name, size)
+
+    results = {
+        "classification": result,
+    }
+
+    response = jsonify(results)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
+   application.run(host="127.0.0.1", port=5002)
